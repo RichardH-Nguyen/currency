a6ab596fea33c62a6c9e68c4b4a399e52464b17c
RHN <stuff@stuff.com>
2018-04-17 15:44:30 -0500

    in the middle of converting with fixer api

--- model/currencyDB.js
+++ model/currencyDB.js
@@ -1,12 +1,33 @@
 /**
  * Created by su7163if on 3/13/2018.
  */
-module.exports = {
-    EURtoUSD: 1.23,
-    EURtoJPY: 130.37,
-    JPYtoUSD: 0.0094,
-    JPYtoEUR: 0.0077,
-    USDtoEUR: 0.81,
-    USDtoJPY: 106.02
+var request = require('request');
 
-};
+var baseURL = 'http://data.fixer.io/api/latest?';
+var apiKey = process.env.FIXER_API_KEY;
+
+function currencyRequest(callback, base, to){
+    process.nextTick(function(){
+
+        queryParam = {access_key: apiKey, base: base, symbols: to};
+
+        request({uri: baseURL, qs: queryParam}, function(error, fixer_response, body){
+
+            if(!error && fixer_response.statusCode === 200){
+                console.log(JSON.stringify(body));
+                var fixerJSON = JSON.parse(body);
+                callback(null, fixerJSON);
+            }
+            else{
+                console.log(error);
+                console.log(apiKey);
+                console.log(fixer_response);
+                callback(Error("Error fetching data"));
+            }
+        });
+
+
+    });
+}
+
+module.exports = currencyRequest;
--- routes/index.js
+++ routes/index.js
@@ -21,24 +21,28 @@ router.get('/convert', function(req, res, next){
 
 
     var amount =req.query.amount;
-    var fromCurrency = req.query.from_currency;
+    var fromCurrency = 'EUR';
     var toCurrency = req.query.to_currency;
     var converted;
-    var convertKey = fromCurrency + "to" + toCurrency;
+    var data = [];
+    //var convertKey = fromCurrency + "to" + toCurrency;
+    exchangeRates(function(err, fixerdata){
+        if(err){
+            res.render('Error', {message: err.message, title: 'error'})
+        }
+        else{
+            console.log(fixerdata.rates[toCurrency]);
+            //converted = fixerdata.rates[0];
+            res.render('results', {converted: fixerdata.rates[toCurrency]});
+        }
+    }, fromCurrency, toCurrency);
 
-    if(fromCurrency === toCurrency){
-        converted = amount;
-    }
-    else{
-        converted = amount * exchangeRates[convertKey]
-    }
 
 
-
-    res.render('results', {
+    /*res.render('results', {
         amount: amount,
         fromCurrency: fromCurrency,
         toCurrency: toCurrency,
-        converted: converted});
+        converted: data.rates[0]});*/
 });
 module.exports = router;
