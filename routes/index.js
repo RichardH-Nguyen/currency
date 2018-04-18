bc2289e68dd432d52982a0501aaeefd073ebb2d1
su7163if <stuff@stuff.com>
2018-04-18 18:28:32 -0500

    need to find a way to go from USD to JPY using EUR as a middle man

--- routes/index.js
+++ routes/index.js
@@ -21,10 +21,12 @@ router.get('/convert', function(req, res, next){
 
 
     var amount =req.query.amount;
-    var fromCurrency = 'EUR';
+    var base = 'EUR';
+    var fromCurrency = req.query.from_currency;
     var toCurrency = req.query.to_currency;
-    var converted;
-    var data = [];
+    var convertKeys = 'USD, JPY, EUR';
+    var convertedAmount;
+
     //var convertKey = fromCurrency + "to" + toCurrency;
     exchangeRates(function(err, fixerdata){
         if(err){
@@ -32,10 +34,27 @@ router.get('/convert', function(req, res, next){
         }
         else{
             console.log(fixerdata.rates[toCurrency]);
+            var convertRate = fixerdata.rates[toCurrency];
+
+            if(fromCurrency === 'EUR'){
+                convertedAmount = amount * convertRate;
+            }
+            else if(toCurrency === 'EUR'){
+                convertRate = fixerdata.rates[fromCurrency];
+                convertedAmount = amount / convertRate;
+                console.log(convertedAmount)
+            }
+            else{
+                // use euro as a middle man to go from USD to JPY and the otherway around
+            }
+
             //converted = fixerdata.rates[0];
-            res.render('results', {converted: fixerdata.rates[toCurrency]});
+            res.render('results', {amount: amount,
+                fromCurrency: fromCurrency,
+                toCurrency: toCurrency,
+                converted: convertedAmount});
         }
-    }, fromCurrency, toCurrency);
+    }, base, convertKeys);
 
 
 
