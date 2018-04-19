7f7b71377589ff30b4b0c798606582753bdf6761
RHN <stuff@stuff.com>
2018-04-19 16:10:56 -0500

    Finished with advanced portion of lab 11

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,27 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+/* GET stuff page. */
 router.get('/stuff', function(req, res, next) {
     res.send("stuff goes here");
 });
 
+/* GET about page. */
 router.get('/about', function(req, res, next){
     res.render('about', {
         name: "Richard Nguyen",
         description: "Converts multiple currencies"});
 });
 
+/* GET convert page. */
 router.get('/convert', function(req, res, next){
 
-
     var amount =req.query.amount;
-    var base = 'EUR';
+    //var base = 'EUR';
     var fromCurrency = req.query.from_currency;
     var toCurrency = req.query.to_currency;
-    var convertKeys = 'USD, JPY, EUR';
     var convertedAmount;
 
-    //var convertKey = fromCurrency + "to" + toCurrency;
     exchangeRates(function(err, fixerdata){
         if(err){
             res.render('Error', {message: err.message, title: 'error'})
@@ -37,31 +37,30 @@ router.get('/convert', function(req, res, next){
             var convertRate = fixerdata.rates[toCurrency];
 
             if(fromCurrency === 'EUR'){
+                // Convert from Euro
                 convertedAmount = amount * convertRate;
             }
             else if(toCurrency === 'EUR'){
+                // Convert to the euro
                 convertRate = fixerdata.rates[fromCurrency];
                 convertedAmount = amount / convertRate;
-                console.log(convertedAmount)
+                //console.log(convertedAmount)
             }
             else{
                 // use euro as a middle man to go from USD to JPY and the otherway around
+                convertRate = fixerdata.rates[fromCurrency];
+                var toEURO = amount / convertRate;
+                convertRate = fixerdata.rates[toCurrency];
+                convertedAmount = toEURO * convertRate;
             }
 
             //converted = fixerdata.rates[0];
             res.render('results', {amount: amount,
                 fromCurrency: fromCurrency,
                 toCurrency: toCurrency,
-                converted: convertedAmount});
+                converted: convertedAmount.toFixed(2)});
         }
-    }, base, convertKeys);
+    });
 
-
-
-    /*res.render('results', {
-        amount: amount,
-        fromCurrency: fromCurrency,
-        toCurrency: toCurrency,
-        converted: data.rates[0]});*/
 });
 module.exports = router;
